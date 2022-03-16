475b600bea34149df3fcb5406ae7fa8d6396b613
Shuning Zhang <shuningz@usc.edu>
2022-03-16 00:26:37 -0700

    finish health form page

--- app/src/main/java/com/example/covider/MainActivity.java
+++ app/src/main/java/com/example/covider/MainActivity.java
@@ -5,11 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.covider.database.ManagerFactory;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+    private View mapButton = null;
+    private View reportButton = null;
+    private View profileButton = null;
+    private View mapView = null;
+    private View reportView = null;
+    private View profileView = null;
+    private final HashMap<String, Boolean> answers = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,22 +27,106 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        System.out.println(System.currentTimeMillis());
         ManagerFactory.initialize(getApplicationContext());
+        initializeNavBottom();
+        initializeAnswers();
+        initializeHealthAnswers();
+    }
+
+    private void initializeNavBottom() {
+        if (mapButton == null) {
+            mapButton = findViewById(R.id.map);
+        }
+        if (reportButton == null) {
+            reportButton = findViewById(R.id.report);
+        }
+        if (profileButton == null) {
+            profileButton = findViewById(R.id.profile);
+        }
+        if (mapView == null) {
+            mapView = findViewById(R.id.map_view);
+        }
+        if (reportView == null) {
+            reportView = findViewById(R.id.report_view);
+        }
+        if (profileView == null) {
+            profileView = findViewById(R.id.profile_view);
+        }
         ImageButton.OnClickListener bottomNavListener = (View view) -> {
             view.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal_selected)));
             String content = view.getContentDescription().toString();
-            if (content.equals("Map")) {
-                findViewById(R.id.report).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-                findViewById(R.id.profile).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-            } else if (content.equals("Report")) {
-                findViewById(R.id.map).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-                findViewById(R.id.profile).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-            } else if (content.equals("Profile")) {
-                findViewById(R.id.report).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-                findViewById(R.id.map).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+            switch (content) {
+                case "Map":
+                    mapView.setVisibility(View.VISIBLE);
+                    reportView.setVisibility(View.INVISIBLE);
+                    reportButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+                    profileButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+                    break;
+                case "Report":
+                    reportView.setVisibility(View.VISIBLE);
+                    mapView.setVisibility(View.INVISIBLE);
+                    mapButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+                    profileButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+                    break;
+                case "Profile":
+                    reportView.setVisibility(View.INVISIBLE);
+                    reportButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+                    mapView.setVisibility(View.INVISIBLE);
+                    mapButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+                    break;
             }
         };
-        findViewById(R.id.map).setOnClickListener(bottomNavListener);
-        findViewById(R.id.report).setOnClickListener(bottomNavListener);
-        findViewById(R.id.profile).setOnClickListener(bottomNavListener);
+        mapButton.setOnClickListener(bottomNavListener);
+        reportButton.setOnClickListener(bottomNavListener);
+        profileButton.setOnClickListener(bottomNavListener);
+    }
+
+    private void initializeAnswers() {
+        answers.put("infection", null);
+        answers.put("chills_fever", null);
+        answers.put("taste_smell", null);
+        answers.put("muscle", null);
+        answers.put("cough", null);
+        answers.put("breathing", null);
+        answers.put("conjunctivitis", null);
+        answers.put("gi_symptoms", null);
+    }
+
+    private void initializeHealthAnswers() {
+        Button.OnClickListener listener = (View view) -> {
+            Button clicked = (Button)view;
+            clicked.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+            clicked.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.gold)));
+            String answer = ((Button)view).getText().toString();
+            String description = view.getContentDescription().toString();
+            if (answer.equals("Yes")) {
+                answers.put(description, true);
+                int oppositeAnswerId = getResources().getIdentifier(description+"_no", "id", getPackageName());
+                Button opposite = findViewById(oppositeAnswerId);
+                opposite.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.switch_off_track)));
+                opposite.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.black)));
+            } else if (answer.equals("No")) {
+                answers.put(description, false);
+                int oppositeAnswerId = getResources().getIdentifier(description+"_yes", "id", getPackageName());
+                Button opposite = findViewById(oppositeAnswerId);
+                opposite.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.switch_off_track)));
+                opposite.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.black)));
+            }
+        };
+        findViewById(R.id.infection_yes).setOnClickListener(listener);
+        findViewById(R.id.infection_no).setOnClickListener(listener);
+        findViewById(R.id.chills_fever_yes).setOnClickListener(listener);
+        findViewById(R.id.chills_fever_no).setOnClickListener(listener);
+        findViewById(R.id.taste_smell_yes).setOnClickListener(listener);
+        findViewById(R.id.taste_smell_no).setOnClickListener(listener);
+        findViewById(R.id.muscle_yes).setOnClickListener(listener);
+        findViewById(R.id.muscle_no).setOnClickListener(listener);
+        findViewById(R.id.cough_yes).setOnClickListener(listener);
+        findViewById(R.id.cough_no).setOnClickListener(listener);
+        findViewById(R.id.breathing_yes).setOnClickListener(listener);
+        findViewById(R.id.breathing_no).setOnClickListener(listener);
+        findViewById(R.id.conjunctivitis_yes).setOnClickListener(listener);
+        findViewById(R.id.conjunctivitis_no).setOnClickListener(listener);
+        findViewById(R.id.gi_symptoms_yes).setOnClickListener(listener);
+        findViewById(R.id.gi_symptoms_no).setOnClickListener(listener);
     }
 }
