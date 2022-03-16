b63ffe006f9bfbd363999cee1057cc211140925f
Shuning Zhang <shuningz@usc.edu>
2022-03-15 21:57:08 -0700

    add navbar onclick event

--- app/src/main/java/com/example/covider/MainActivity.java
+++ app/src/main/java/com/example/covider/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.covider;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.covider.database.ManagerFactory;
 
@@ -14,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        System.out.println(System.currentTimeMillis());
         ManagerFactory.initialize(getApplicationContext());
+        ImageButton.OnClickListener bottomNavListener = (View view) -> {
+            view.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal_selected)));
+            String content = view.getContentDescription().toString();
+            if (content.equals("Map")) {
+                findViewById(R.id.report).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+                findViewById(R.id.profile).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+            } else if (content.equals("Report")) {
+                findViewById(R.id.map).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+                findViewById(R.id.profile).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+            } else if (content.equals("Profile")) {
+                findViewById(R.id.report).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+                findViewById(R.id.map).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+            }
+        };
+        findViewById(R.id.map).setOnClickListener(bottomNavListener);
+        findViewById(R.id.report).setOnClickListener(bottomNavListener);
+        findViewById(R.id.profile).setOnClickListener(bottomNavListener);
     }
 }
