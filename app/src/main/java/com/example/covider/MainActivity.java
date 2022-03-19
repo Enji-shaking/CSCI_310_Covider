79291f18fa552216ad318770ce3df649a95e785d
Shuning Zhang <shuningz@usc.edu>
2022-03-18 23:53:57 -0700

    add more buildings
    add more buildings

--- app/src/main/java/com/example/covider/MainActivity.java
+++ app/src/main/java/com/example/covider/MainActivity.java
@@ -6,8 +6,13 @@ import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.covider.database.ManagerFactory;
@@ -31,115 +36,121 @@ public class MainActivity extends AppCompatActivity {
 //        System.out.println(System.currentTimeMillis());
         getApplicationContext().deleteDatabase("covider"); // clear database for debug use
         ManagerFactory.initialize(getApplicationContext());
-        initializeNavBottom();
-        initializeAnswers();
-        initializeHealthAnswers();
-        initializeUserProfile();
+//        initializeNavBottom();
+//        initializeAnswers();
+//        initializeHealthAnswers();
+//        initializeUserProfile();
+        ScrollView vertical = findViewById(R.id.usc_map_vertical);
+        HorizontalScrollView horizontal = findViewById(R.id.usc_map_horizontal);
+        horizontal.getChildAt(0).getViewTreeObserver().addOnGlobalLayoutListener(() ->
+                horizontal.scrollTo(horizontal.getChildAt(0).getWidth()/2,0));
+        vertical.getChildAt(0).getViewTreeObserver().addOnGlobalLayoutListener(() ->
+                vertical.scrollTo(0, vertical.getChildAt(0).getHeight()/2));
     }
 
-    private void initializeNavBottom() {
-        if (mapButton == null) {
-            mapButton = findViewById(R.id.map);
-        }
-        if (reportButton == null) {
-            reportButton = findViewById(R.id.report);
-        }
-        if (profileButton == null) {
-            profileButton = findViewById(R.id.profile);
-        }
-        if (mapView == null) {
-            mapView = findViewById(R.id.map_view);
-        }
-        if (reportView == null) {
-            reportView = findViewById(R.id.report_view);
-        }
-        if (profileView == null) {
-            profileView = findViewById(R.id.profile_view);
-        }
-        ImageButton.OnClickListener bottomNavListener = (View view) -> {
-            view.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal_selected)));
-            String content = view.getContentDescription().toString();
-            switch (content) {
-                case "Map":
-                    mapView.setVisibility(View.VISIBLE);
-                    reportView.setVisibility(View.INVISIBLE);
-                    profileView.setVisibility(View.INVISIBLE);
-                    reportButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-                    profileButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-                    break;
-                case "Report":
-                    reportView.setVisibility(View.VISIBLE);
-                    mapView.setVisibility(View.INVISIBLE);
-                    profileView.setVisibility(View.INVISIBLE);
-                    mapButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-                    profileButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-                    break;
-                case "Profile":
-                    profileView.setVisibility(View.VISIBLE);
-                    reportView.setVisibility(View.INVISIBLE);
-                    mapView.setVisibility(View.INVISIBLE);
-                    reportButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-                    mapButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-                    break;
-            }
-        };
-        mapButton.setOnClickListener(bottomNavListener);
-        reportButton.setOnClickListener(bottomNavListener);
-        profileButton.setOnClickListener(bottomNavListener);
-    }
-
-    private void initializeAnswers() {
-        answers.put("infection", null);
-        answers.put("chills_fever", null);
-        answers.put("taste_smell", null);
-        answers.put("muscle", null);
-        answers.put("cough", null);
-        answers.put("breathing", null);
-        answers.put("conjunctivitis", null);
-        answers.put("gi_symptoms", null);
-    }
-
-    private void initializeHealthAnswers() {
-        Button.OnClickListener listener = (View view) -> {
-            Button clicked = (Button)view;
-            clicked.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
-            clicked.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.gold)));
-            String answer = ((Button)view).getText().toString();
-            String description = view.getContentDescription().toString();
-            if (answer.equals("Yes")) {
-                answers.put(description, true);
-                int oppositeAnswerId = getResources().getIdentifier(description+"_no", "id", getPackageName());
-                Button opposite = findViewById(oppositeAnswerId);
-                opposite.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.switch_off_track)));
-                opposite.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.black)));
-            } else if (answer.equals("No")) {
-                answers.put(description, false);
-                int oppositeAnswerId = getResources().getIdentifier(description+"_yes", "id", getPackageName());
-                Button opposite = findViewById(oppositeAnswerId);
-                opposite.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.switch_off_track)));
-                opposite.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.black)));
-            }
-        };
-        findViewById(R.id.infection_yes).setOnClickListener(listener);
-        findViewById(R.id.infection_no).setOnClickListener(listener);
-        findViewById(R.id.chills_fever_yes).setOnClickListener(listener);
-        findViewById(R.id.chills_fever_no).setOnClickListener(listener);
-        findViewById(R.id.taste_smell_yes).setOnClickListener(listener);
-        findViewById(R.id.taste_smell_no).setOnClickListener(listener);
-        findViewById(R.id.muscle_yes).setOnClickListener(listener);
-        findViewById(R.id.muscle_no).setOnClickListener(listener);
-        findViewById(R.id.cough_yes).setOnClickListener(listener);
-        findViewById(R.id.cough_no).setOnClickListener(listener);
-        findViewById(R.id.breathing_yes).setOnClickListener(listener);
-        findViewById(R.id.breathing_no).setOnClickListener(listener);
-        findViewById(R.id.conjunctivitis_yes).setOnClickListener(listener);
-        findViewById(R.id.conjunctivitis_no).setOnClickListener(listener);
-        findViewById(R.id.gi_symptoms_yes).setOnClickListener(listener);
-        findViewById(R.id.gi_symptoms_no).setOnClickListener(listener);
-    }
-
-    private void initializeUserProfile() {
-        ((TextView)findViewById(R.id.username)).setText(Html.fromHtml("Hi, <b>Trojan</b>!"));
-
-    }
+//    private void initializeNavBottom() {
+//        if (mapButton == null) {
+//            mapButton = findViewById(R.id.map);
+//        }
+//        if (reportButton == null) {
+//            reportButton = findViewById(R.id.report);
+//        }
+//        if (profileButton == null) {
+//            profileButton = findViewById(R.id.profile);
+//        }
+//        if (mapView == null) {
+//            mapView = findViewById(R.id.map_view);
+//        }
+//        if (reportView == null) {
+//            reportView = findViewById(R.id.report_view);
+//        }
+//        if (profileView == null) {
+//            profileView = findViewById(R.id.profile_view);
+//        }
+//        ImageButton.OnClickListener bottomNavListener = (View view) -> {
+//            view.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal_selected)));
+//            String content = view.getContentDescription().toString();
+//            switch (content) {
+//                case "Map":
+//                    mapView.setVisibility(View.VISIBLE);
+//                    reportView.setVisibility(View.INVISIBLE);
+//                    profileView.setVisibility(View.INVISIBLE);
+//                    reportButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+//                    profileButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+//                    break;
+//                case "Report":
+//                    reportView.setVisibility(View.VISIBLE);
+//                    mapView.setVisibility(View.INVISIBLE);
+//                    profileView.setVisibility(View.INVISIBLE);
+//                    mapButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+//                    profileButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+//                    break;
+//                case "Profile":
+//                    profileView.setVisibility(View.VISIBLE);
+//                    reportView.setVisibility(View.INVISIBLE);
+//                    mapView.setVisibility(View.INVISIBLE);
+//                    reportButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+//                    mapButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+//                    break;
+//            }
+//        };
+//        mapButton.setOnClickListener(bottomNavListener);
+//        reportButton.setOnClickListener(bottomNavListener);
+//        profileButton.setOnClickListener(bottomNavListener);
+//    }
+//
+//    private void initializeAnswers() {
+//        answers.put("infection", null);
+//        answers.put("chills_fever", null);
+//        answers.put("taste_smell", null);
+//        answers.put("muscle", null);
+//        answers.put("cough", null);
+//        answers.put("breathing", null);
+//        answers.put("conjunctivitis", null);
+//        answers.put("gi_symptoms", null);
+//    }
+//
+//    private void initializeHealthAnswers() {
+//        Button.OnClickListener listener = (View view) -> {
+//            Button clicked = (Button)view;
+//            clicked.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.cardinal)));
+//            clicked.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.gold)));
+//            String answer = ((Button)view).getText().toString();
+//            String description = view.getContentDescription().toString();
+//            if (answer.equals("Yes")) {
+//                answers.put(description, true);
+//                int oppositeAnswerId = getResources().getIdentifier(description+"_no", "id", getPackageName());
+//                Button opposite = findViewById(oppositeAnswerId);
+//                opposite.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.switch_off_track)));
+//                opposite.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.black)));
+//            } else if (answer.equals("No")) {
+//                answers.put(description, false);
+//                int oppositeAnswerId = getResources().getIdentifier(description+"_yes", "id", getPackageName());
+//                Button opposite = findViewById(oppositeAnswerId);
+//                opposite.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.switch_off_track)));
+//                opposite.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.black)));
+//            }
+//        };
+//        findViewById(R.id.infection_yes).setOnClickListener(listener);
+//        findViewById(R.id.infection_no).setOnClickListener(listener);
+//        findViewById(R.id.chills_fever_yes).setOnClickListener(listener);
+//        findViewById(R.id.chills_fever_no).setOnClickListener(listener);
+//        findViewById(R.id.taste_smell_yes).setOnClickListener(listener);
+//        findViewById(R.id.taste_smell_no).setOnClickListener(listener);
+//        findViewById(R.id.muscle_yes).setOnClickListener(listener);
+//        findViewById(R.id.muscle_no).setOnClickListener(listener);
+//        findViewById(R.id.cough_yes).setOnClickListener(listener);
+//        findViewById(R.id.cough_no).setOnClickListener(listener);
+//        findViewById(R.id.breathing_yes).setOnClickListener(listener);
+//        findViewById(R.id.breathing_no).setOnClickListener(listener);
+//        findViewById(R.id.conjunctivitis_yes).setOnClickListener(listener);
+//        findViewById(R.id.conjunctivitis_no).setOnClickListener(listener);
+//        findViewById(R.id.gi_symptoms_yes).setOnClickListener(listener);
+//        findViewById(R.id.gi_symptoms_no).setOnClickListener(listener);
+//    }
+//
+//    private void initializeUserProfile() {
+//        ((TextView)findViewById(R.id.username)).setText(Html.fromHtml("Hi, <b>Trojan</b>!"));
+//
+//    }
 }
