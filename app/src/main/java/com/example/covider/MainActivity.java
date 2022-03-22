44cab4912877b80f5405d44bf1db8499a46247ce
Shuning Zhang <shuningz@usc.edu>
2022-03-21 23:40:24 -0700

    finish list view, print building fullname and code onclick

--- app/src/main/java/com/example/covider/MainActivity.java
+++ app/src/main/java/com/example/covider/MainActivity.java
@@ -162,13 +162,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void initializeMapView() {
         initializeMapBuildings();
-        Switch viewToggleSwitch = findViewById(R.id.toggle_view);
         if (buildingsMap == null) {
             buildingsMap = findViewById(R.id.usc_map_view);
         }
         if (buildingsList == null) {
             buildingsList = findViewById(R.id.usc_list_view);
         }
+        Switch viewToggleSwitch = findViewById(R.id.toggle_view);
         viewToggleSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
                 buildingsMap.setVisibility(View.VISIBLE);
@@ -178,6 +178,7 @@ public class MainActivity extends AppCompatActivity {
                 buildingsList.setVisibility(View.VISIBLE);
             }
         });
+        initializeListBuildings();
     }
 
     private void initializeMapBuildings() {
@@ -185,8 +186,8 @@ public class MainActivity extends AppCompatActivity {
             String code = view.getContentDescription().toString();
             int stringId = getResources().getIdentifier(
                     code + "_full", "string", getPackageName());
-            String mess = getResources().getString(stringId);
-            System.out.println(mess);
+            String fullName = getResources().getString(stringId);
+            System.out.println(fullName);
         };
         findViewById(R.id.usc_map_esh).setOnClickListener(buildingListener);
         findViewById(R.id.usc_map_mcc).setOnClickListener(buildingListener);
@@ -325,5 +326,151 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.usc_map_mrc).setOnClickListener(buildingListener);
         findViewById(R.id.usc_map_cic).setOnClickListener(buildingListener);
         findViewById(R.id.usc_map_mhc).setOnClickListener(buildingListener);
+    }
+
+    private void initializeListBuildings() {
+        LinearLayout.OnClickListener buildingListener = (View view) -> {
+            String code = view.getContentDescription().toString();
+            int stringId = getResources().getIdentifier(
+                    code + "_display", "string", getPackageName());
+            String displayName = getResources().getString(stringId);
+            System.out.println(displayName);
+        };
+        findViewById(R.id.usc_list_esh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_mcc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_flt).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_koh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_wto).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_lrc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_uac).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_jef).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_den).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_kdc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_urc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_clh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_uuc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ahn).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_jep).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ksh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_swc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_mrf).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ois).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_dcc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_mtx).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_drc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_sci).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_scb).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_scc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_sca).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_scx).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_sce).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_aes).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ctv).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_jws).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_tmc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_mus).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_rhm).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_bmh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_thh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_cas).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_wph).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_sos).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_lvl).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_jmc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_gap).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_her).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_lts).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ctf).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_mar).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_drb).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_kap).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_rri).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_mcb).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_sgm).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_gfs).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_hnb).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_dps).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_scd).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ttl).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ger).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_pks).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_prb).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_eeb).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_rth).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ohe).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_pce).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_hce).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_bhe).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ssc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_sal).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_sel).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_phe).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_vhe).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_iyh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_wah).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_lhi).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_rrb).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ann).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ped).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_asc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_bit).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_nct).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_adm).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_bks).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_sto).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_hsh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_tcc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_stu).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_sks).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_cem).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ljs).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_zhs).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_slh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_acb).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_har).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_mhp).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_bri).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_acc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_hoh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ahf1).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_hmm).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ahf2).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_jhh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_tgf).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_law).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_vpd).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_rgl).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_dml).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_cpa).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_bsr).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_nrc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ptd).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_dxm).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_alm).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_dmt).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_tro).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_fig).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_jkp).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_jff).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_rmh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ugb).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_gec).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ugw).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_shr).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_elb).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_cdf).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_cal).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_ift).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_rzc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_scs).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_trh).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_huc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_hjc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_osp).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_crc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_nbc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_mrc).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_cic).setOnClickListener(buildingListener);
+        findViewById(R.id.usc_list_mhc).setOnClickListener(buildingListener);
     }
 }
