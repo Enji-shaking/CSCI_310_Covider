3324cfbe8d8df672eee7d4fa4575cb6c249acd75
Shuning Zhang <shuningz@usc.edu>
2022-04-03 23:23:56 -0700

    add login page test

---
+++ app/src/androidTest/java/com/example/covider/frontend/LogInViewInstrumentedTest.java
@@ -0,0 +1,136 @@
+package com.example.covider.frontend;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.covider.R;
+import com.example.covider.MainActivity;
+import com.example.covider.database.ManagerFactory;
+import com.example.covider.database.user.UserManager;
+import com.example.covider.model.user.User;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LogInViewInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void createUser(){
+        // add users
+        UserManager userManager = ManagerFactory.getUserManagerInstance();
+        userManager.addOrUpdateUser(new User(100,"Enji", "Aa12345678", 1));
+    }
+
+    @Test
+    public void TestInitialView() {
+        checkIsVisible(R.id.log_in_view);
+        checkIsGone(R.id.nav);
+        checkIsGone(R.id.map_view);
+        checkIsGone(R.id.report_view);
+        checkIsGone(R.id.profile_view);
+        checkIsGone(R.id.notification_view);
+    }
+
+    @Test
+    public void TestNonexistentUser() {
+        // no input
+        onView(withId(R.id.log_in_submit)).perform(click());
+        checkLogInDialogWithText("User does not exist");
+
+        // wrong username without password
+        onView(withId(R.id.log_in_username))
+                .perform(clearText(), replaceText("abc"));
+        onView(withId(R.id.log_in_submit)).perform(click());
+        checkLogInDialogWithText("User does not exist");
+
+        // wrong random username with random password
+        onView(withId(R.id.log_in_username))
+                .perform(clearText(), replaceText("abc"));
+        onView(withId(R.id.log_in_password))
+                .perform(clearText(), replaceText("123"));
+        onView(withId(R.id.log_in_submit)).perform(click());
+        checkLogInDialogWithText("User does not exist");
+
+        // wrong username case with correct password
+        onView(withId(R.id.log_in_username))
+                .perform(clearText(), replaceText("enji"));
+        onView(withId(R.id.log_in_password))
+                .perform(clearText(), replaceText("Aa12345678"));
+        onView(withId(R.id.log_in_submit)).perform(click());
+        checkLogInDialogWithText("User does not exist");
+    }
+
+    @Test
+    public void TestWrongPassword() {
+        onView(withId(R.id.log_in_username))
+                .perform(clearText(), replaceText("Enji"));
+        // no password input
+        onView(withId(R.id.log_in_submit)).perform(click());
+        checkLogInDialogWithText("Wrong Password");
+
+        // wrong password
+        onView(withId(R.id.log_in_password))
+                .perform(clearText(), replaceText("123"));
+        onView(withId(R.id.log_in_submit)).perform(click());
+        checkLogInDialogWithText("Wrong Password");
+
+        // wrong password case
+        onView(withId(R.id.log_in_password))
+                .perform(clearText(), replaceText("aa12345678"));
+        onView(withId(R.id.log_in_submit)).perform(click());
+        checkLogInDialogWithText("Wrong Password");
+    }
+
+    @Test
+    public void TestLogInSuccess() {
+        onView(withId(R.id.log_in_username))
+                .perform(clearText(), replaceText("Enji"));
+        onView(withId(R.id.log_in_password))
+                .perform(clearText(), replaceText("Aa12345678"));
+        onView(withId(R.id.log_in_submit)).perform(click());
+        checkIsGone(R.id.log_in_view);
+        checkIsVisible(R.id.nav);
+        checkIsVisible(R.id.map_view);
+        checkIsGone(R.id.report_view);
+        checkIsGone(R.id.profile_view);
+        checkIsGone(R.id.notification_view);
+        onView(withId(R.id.toggle_view)).check(matches(isChecked()));
+        onView(withId(R.id.username))
+                .check(matches(withText("Hi, Enji!")));
+    }
+
+    private void checkLogInDialogWithText(String message) {
+        ViewInteraction dialog = onView(withText("Error"));
+        dialog
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()));
+        onView(withText(message))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()));
+        onView(withText("Cancel")).perform(click());
+        onView(isRoot()).inRoot(isDialog()).noActivity();
+    }
+
+    private void checkIsVisible(int id) {
+        onView(withId(id)).check(matches(ViewMatchers.withEffectiveVisibility(Visibility.VISIBLE)));
+    }
+
+    private void checkIsGone(int id) {
+        onView(withId(id)).check(matches(ViewMatchers.withEffectiveVisibility(Visibility.GONE)));
+    }
+}
